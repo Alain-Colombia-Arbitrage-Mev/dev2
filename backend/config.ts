2f776c6e8b7aee930d79963e52a283ea931fb8e0
Alain-Colombia-Arbitrage-Mev <lugiraldo2019@gmail.com>
2025-04-14 22:00:22 -0500

    Commit en el frontend

--- backend/config.ts
+++ backend/config.ts
@@ -1,3 +1,4 @@
 // The base url for where the frontend is hosted.
 // Update to point to where you are hosting your frontend.
 export const FRONTEND_URL = "https://encorets-saas-starter.vercel.app";
+//encore app
